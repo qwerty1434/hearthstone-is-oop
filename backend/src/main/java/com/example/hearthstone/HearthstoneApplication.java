6139e4dbbe58b07e3128c54e8c4d7b94286b2658
qwerty1434 <qwerty1434@naver.com>
2024-07-06 07:30:04 +0900

    :tada: 프로젝트 생성

---
+++ backend/src/main/java/com/example/hearthstone/HearthstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.hearthstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearthstoneApplication.class, args);
+	}
+
+}
